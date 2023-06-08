bea66d9dca73f2fb7ba49ab0bad5f168c94d8991
EdielD7 <129358065+EdielD7@users.noreply.github.com>
2023-06-08 11:08:29 -0400

    Criado script.js
    Criação de script.sj e vinculado a html.
    Acrescentado placeholders aos inputs.

---
+++ script.js
@@ -0,0 +1,9 @@
+function validar() {
+    let tnum1 = document.getElementById(txtnum1)
+    let tnum2 = document.getElementById(txtnum2)
+    let n1 = parseInt(tnum1)
+    let n2 = parseInt(tnum2)
+    if (n1 < n2) {
+        
+    }
+}
