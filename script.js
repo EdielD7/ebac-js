81129567e5af1494d52fe6251d0d86c47595bcc8
EdielD7 <129358065+EdielD7@users.noreply.github.com>
2023-06-20 18:21:09 -0400

    Iniciado validação

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let f = document.getElementById('fcompare')
+f.addEventListener('submit', submeter)
+
+let tnum1 = document.getElementById('txtnum1')
+let tnum2 = document.getElementById('txtnum2')
+let n1 = parseInt(tnum1)
+let n2 = parseInt(tnum2)
 function validar() {
-    let tnum1 = document.getElementById(txtnum1)
-    let tnum2 = document.getElementById(txtnum2)
-    let n1 = parseInt(tnum1)
-    let n2 = parseInt(tnum2)
+    let valido = document.getElementById('validador')
     if (n1 < n2) {
-        
+        valido.innerHTML = '<p style="display:flex; text-align: center;">É VÁLIDO!</p>'
+        valido.style.display='block'
     }
 }
